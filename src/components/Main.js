0d0138ba876cc82fce99ed650046bded1327fb39
Paty Varela <patrela@hotmail.com>
2023-09-20 19:44:55 -0500

    layout essentials

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+export default function Main({sectionTitle, boxes, boxImage}) {
+  return (
+    <main>
+    <div id='section-title'>Popular Titles</div>
+    <h2> prueba container</h2>
+    <section className='container'>
+      <div className='box'>
+        <div className='section-box-title'>MOVIES</div>
+      </div>
+      <div>caja 2</div>
+      <div>caja 3</div>
+    </section>  
+    </main>
+  );
+}
